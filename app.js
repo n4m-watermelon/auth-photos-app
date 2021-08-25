d9d8ef4ee5b9ec945031641b424e1fa64f364a87
leDoanNam <namthamlam505@gmail.com>
2021-08-25 15:56:48 +0700

    up port

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json())
 // routes
 app.use('/api/user', authRouter)
 app.use('/post', postsRouter)
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port , ()=>{
     console.log(`Server listening on ${port} !`)
 })
